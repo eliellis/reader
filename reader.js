9e798f16b405918004442728ad755b85eab6ad48
Eli Ellis <me@eliellis.com>
2013-01-12 15:40:55 -0500

    Made search more reliable

--- reader.js
+++ reader.js
@@ -2,6 +2,21 @@ cheerio = require('cheerio');
 async = require('async');
 
 var MAXRANKING = 100;
+var reader = {
+  negative: ['share','head','foot','ad','clearfix','widget','navigat','comment', 'image', 'media', 'meta', 'promo', 'rss', 'shoutbox', 'sponsor', 'side'],
+  positive: ['instapaper_body', 'body', 'page', 'content', 'contents,', 'text', 'article', 'blog', 'post', 'main', 'story-body', 'article-body', 'post-body']
+};
+
+function clean(item, callback){
+    $(item).find('noscript').remove();
+    $(item).find('script').remove();
+    $(item).find('iframe').remove();
+    for (var i in reader.negative){
+      $(item).find('[class*=' + reader.negative[i] + ']').remove();
+      $(item).find('[id*=' + reader.negative[i] + ']').remove();
+    }
+    callback(null, item);
+}
 
 function fuzzyTestElement(strings){
   var list = [];
@@ -29,44 +44,42 @@ function strictTestElement(strings){
 */
 module.exports = function(context, finished){
   $ = cheerio.load(context);
-  var div_list = [];
+  var el_list = [];
 
-  async.forEach($('div'),
+  async.forEach($('*'),
     function(item, callback){
-      var testEl = fuzzyTestElement.bind(item); //bind the 'this' keyword to the current div
-      if (testEl(['body', 'content', 'text', 'article', 'blog', 'post'])){
-        div_list.push($(item));
+      var testEl = fuzzyTestElement.bind(item); //bind the 'this' keyword to the current element
+      if (testEl(reader.positive)){
+        el_list.push($(item));
       }
       callback(null);
     },
     function(err){
       //removes extraneouos tags and maps all the items to cheerio objects
-      async.map(div_list,
+      async.map(el_list,
         function(item, callback){
-          $(item).find('[class~=image]').remove();
-          $(item).find('noscript').remove();
-          $(item).find('script').remove();
-          $(item).find('iframe').remove();
-          $(item).find('[class*=navigat]').remove();
-          $(item).find('[class*=comment]').remove();
-          $(item).find('[id*=comment]').remove();
-          callback(null, item);
+          clean(item, callback);
         },
         function(err, results){
           async.sortBy(results, //sorts elements by calculated likelyhood that they are actually the content
             function(item, callback){
               var rank = 0;
-              var search_tags = ['instapaper_body', 'body', 'content', 'contents,', 'text', 'article', 'blog', 'post', 'main', 'story-body', 'article-body', 'post-body'];
-
-              //someplace, somewhere these words show up in the id or class of the div, and the more the merrier
-              rank += MAXRANKING * fuzzyTestElement.call(item, search_tags).length;
-
-              //as sure as we can be that this is the content we're looking for, since this is a strict test, we add more (+75%) to the rank
-              if (strictTestElement.call(item, search_tags)){
-                rank += (MAXRANKING / 4 * 3);
+              var itemText = item.text();
+              //someplace, somewhere these words show up in the id or class of the element, and the more the merrier
+              rank += fuzzyTestElement.call(item, reader.positive).length;
+              if (strictTestElement.call(item, reader.positive)){
+                rank += 10;
               }
-
-              var weight = -(rank * item.text().length);
+              rank += Math.min(Math.floor(itemText.length / 250), 5); //for every 250 characters, add an additional point to the rank, up to 5
+              rank += Math.min(Math.floor($(item).find('p').length / 10), 5); //for every <p> element, add an additional point to the rank, up to 5
+              rank += Math.min(Math.floor($(item).find('br').length / 10), 5); //for every <br> element, add an additional point to the rank, up to 5
+              var punctuation = itemText.match(/[.,!?"]/g); //add points for all punctuation characters
+              rank += (punctuation) ? punctuation.length : 0;
+              if (itemText.length < 10){
+                rank = 0;
+              }
+              var weight = -(rank);
+              console.log(weight, punctuation, itemText);
               callback(null, weight); //negative value because we want the largest weighing item at the top
             },
             function(err, results){
